6e0e84327b07db219a3dd14fc8a48912b807eb93
micky387 <mickaelsaibi@free.fr>
2016-02-22 16:24:03 +0100

    Update for TWRP 3.0 and CM-13.0

--- cm.mk
+++ cm.mk
@@ -7,6 +7,10 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit device configuration
 $(call inherit-product, device/samsung/tblte/device.mk)
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/twrp.fstab:recovery/root/etc/twrp.fstab \
+    $(LOCAL_PATH)/fstab.qcom:recovery/root/fstab.qcom
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := tblte
 PRODUCT_NAME := cm_tblte
--- device.mk
+++ device.mk
@@ -3,11 +3,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
-$(call inherit-product-if-exists, vendor/samsung/trlte/trlte-vendor.mk)
+$(call inherit-product-if-exists, vendor/samsung/tblte/tblte-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/samsung/tblte/overlay
 
 $(call inherit-product, build/target/product/full.mk)
+
+LOCAL_PATH := device/samsung/tblte
 
 PRODUCT_NAME := samsung_tblte
 PRODUCT_BRAND := Samsung
